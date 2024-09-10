263762dc594d72a609fcd01a1839bafd1844453c
Graham Duthie <grahamd483@gmail.com>
2024-09-10 20:54:41 +0200

    Add Franc factory method and update tests

--- src/test/java/com/madeby/fun/currency/MoneyTest.java
+++ src/test/java/com/madeby/fun/currency/MoneyTest.java
@@ -1,6 +1,7 @@
 package com.madeby.fun.currency;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class MoneyTest
 {
@@ -17,23 +18,23 @@ class MoneyTest
 @org.junit.jupiter.api.Test
     void TestFrancMultiplication( )
     {
-        Franc five = new Franc(5);
+        Money five = Money.franc(5);
 
-        assertEquals(new Franc(10), five.times(2));
+        assertEquals(Money.franc(10), five.times(2));
 
-        assertEquals(new Franc(15), five.times(3));
+        assertEquals(Money.franc(15), five.times(3));
     }
     @org.junit.jupiter.api.Test
     void testEquality() {
 
         assertEquals(Money.dollar(5), Money.dollar(5));
-        assertEquals(new Franc(5), new Franc(5));
+        assertEquals(Money.franc(5), Money.franc(5));
 
         assertNotEquals(Money.dollar(5), Money.dollar(6));
-        assertNotEquals(new Franc(5), new Franc(6));
+        assertNotEquals(Money.franc(5), Money.franc(6));
 
-        assertNotEquals(Money.dollar(5), new Franc(5));
-        assertNotEquals(new Franc(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.franc(5));
+        assertNotEquals(Money.franc(5), Money.dollar(5));
         
 
     }
