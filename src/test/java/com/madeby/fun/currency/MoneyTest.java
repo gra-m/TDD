f168a116669dc06498ff60789bac092bb3d4435e
Graham Duthie <grahamd483@gmail.com>
2024-09-10 22:48:22 +0200

    Add plus method to Money class
    Implemented a plus method to add two Money instances. Added a unit test to verify the functionality of this new addition method.

--- src/test/java/com/madeby/fun/currency/MoneyTest.java
+++ src/test/java/com/madeby/fun/currency/MoneyTest.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class MoneyTest
 {
+    @org.junit.jupiter.api.Test
+    void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
 
     @org.junit.jupiter.api.Test
     void TestCurrency( )
